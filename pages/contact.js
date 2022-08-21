b2ff9cb99e5401acc6f619cd95cea95c82b85b06
Wil Hesk <wilhesk@gmail.com>
2022-08-21 23:03:51 +0100

    pagesfavicon

---
+++ pages/contact.js
@@ -0,0 +1,5 @@
+const Contact = () => {
+    return ( <p>Contact Us</p> );
+}
+ 
+export default Contact;
---
+++ pages/portfolio.js
@@ -0,0 +1,5 @@
+const Portfolio = () => {
+    return ( <p>Our Work</p> );
+}
+ 
+export default Portfolio;
