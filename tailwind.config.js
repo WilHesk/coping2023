c9f12dac9d5b2a924228d7979586261725a7bd56
Wil Hesk <wilhesk@gmail.com>
2022-08-25 14:03:26 +0100

    linkandstyle

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'pink': '#ed728e',
+      'ltgrey': '#dddede',
+      'midgrey': '#a2a09f',
+      'dkgrey': '#20232b',
+      'ltblue': '#99c0df',
+      'dkblue': '#17415b',
+    },
     extend: {},
   },
   plugins: [],
