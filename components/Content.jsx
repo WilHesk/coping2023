252c8f4c45104ba52429623a951f0763e5d6896f
Wil Hesk <wilhesk@gmail.com>
2023-06-16 20:35:43 +0100

    pre festival edits

--- components/Content.jsx
+++ components/Content.jsx
@@ -6,32 +6,32 @@ const Content = ({heading, subhead, time1, event1, time2, event2, time3, event3,
     <div className='flex items-center justify-center h-auto pt-20 pb-20 mb-12 bg-fixed bg-center bg-dkgrey'>
         
             <div className='text-white '>
-              <h2 className='text-5xl font-normal text-white '>{heading}</h2>
-              <p className='py-2 text-xl text-pink'>{subhead}</p>
+              <h2 className='pl-4 pr-4 pt-4 pb-2 text-5xl font-normal text-white '>{heading}</h2>
+              <p className='pl-4 pr-20 text-xl font-thin text-pink'>{subhead}</p>
               <div className='mt-[2rem]'>
-              <p className='py-5 pb-0 text-xl '>{time1}</p>
-              <p className='py-5 pt-0 font-thin '>{event1}</p>
-              <p className='py-5 pb-0 text-xl '>{time2}</p>
-              <p className='py-5 pt-0 font-thin '>{event2}</p>
-              <p className='py-5 pb-0 text-xl '>{time3}</p>
-              <p className='py-5 pt-0 font-thin '>{event3}</p>
-              <p className='py-5 pb-0 text-xl '>{time4}</p>
-              <p className='py-5 pt-0 font-thin '>{event4}</p>
-              <p className='py-5 pb-0 text-xl '>{time5}</p>
-              <p className='py-5 pt-0 font-thin '>{event5}</p>
-              <p className='py-5 pb-0 text-xl '>{time6}</p>
-              <p className='py-5 pt-0 font-thin '>{event6}</p>
+              <p className='pl-4 py-5 pb-0 pr-20 text-xl '>{time1}</p>
+              <p className='pl-4 py-5 pt-0 pr-20 font-thin '>{event1}</p>
+              <p className='pl-4 py-5 pb-0 pr-20 text-xl '>{time2}</p>
+              <p className='pl-4 py-5 pt-0 pr-20 font-thin '>{event2}</p>
+              <p className='pl-4 py-5 pb-0 pr-20 text-xl '>{time3}</p>
+              <p className='pl-4 py-5 pt-0 pr-20 font-thin '>{event3}</p>
+              <p className='pl-4 py-5 pb-0 pr-20 text-xl '>{time4}</p>
+              <p className='pl-4 py-5 pt-0 pr-20 font-thin '>{event4}</p>
+              <p className='pl-4 py-5 pb-0 pr-20 text-xl '>{time5}</p>
+              <p className='pl-4 py-5 pt-0 pr-20 font-thin '>{event5}</p>
+              <p className='pl-4 py-5 pb-0 pr-20 text-xl '>{time6}</p>
+              <p className='pl-4 py-5 pt-0 pr-20 font-thin '>{event6}</p>
             </div>
 
             <div className='pt-10'>
               <Link href='https://www.graystoneactionsports.co.uk/graystonenewsblog/coping-2023'>
               <a target="_blank">
-              <button className='px-8 py-2 rounded mr-10 bg-pink text-dkgrey'>{button}</button>
+              <button className=' m-4 px-8 py-2 rounded bg-pink text-dkgrey hover:bg-white'>{button}</button>
               </a>
               </Link>
               <Link href='https://www.jumblebee.co.uk/copingfestival2023auction'>
               <a target="_blank">
-              <button className='px-8 py-2 rounded bg-pink text-dkgrey'>{button2}</button>
+              <button className='m-4 px-8 py-2 rounded bg-pink text-dkgrey hover:bg-white '>{button2}</button>
               </a>
               </Link>
             </div>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,29 +3,24 @@ import Link from 'next/link';
 
 const Hero = ({heading, message, button, button2, heading2, message2 }) => {
   return (
-    <div className='flex items-center justify-center h-auto mb-1 bg-fixed bg-center bg-cover custom-img'>
-        {/* Overlay */}
-        <div className='absolute top-0 left-0 right-0 bottom-0 bg-dkgrey/70 z-[2]' />
-        <div className='p-5 text-white z-[2] mt-[15rem] mr-[20%] ml-[20%]'>
+    <div className='flex items-center justify-center h-screen mb-0 bg-fixed bg-center bg-cover custom-img'>
+        
+        <div className='p-5 text-white mt-[8rem] mr-[10%] ml-[10%]'>
             <div>
               <h2 className='text-5xl font-normal '>{heading}</h2>
-              <p className='py-5 text-xl '>{message}</p>
+              <p className='pt-1 pb-12 text-xl font-thin '>{message}</p>
             </div>
             <div>
               <Link href='https://www.graystoneactionsports.co.uk/graystonenewsblog/coping-2023'>
               <a target="_blank">
-              <button className='mt-5 rounded px-8 py-2 mr-10  bg-pink'>{button}</button>
+              <button className='mt-5 rounded px-8 py-2 mr-10  bg-pink text-dkgrey hover:bg-white'>{button}</button>
               </a>
               </Link>
               <Link href='https://www.jumblebee.co.uk/copingfestival2023auction'>
               <a target="_blank">
-              <button className='mt-5 rounded px-8 py-2 bg-pink'>{button2}</button>
+              <button className='mt-5 rounded px-8 py-2 bg-pink text-dkgrey hover:bg-white'>{button2}</button>
               </a>
               </Link>
-            </div>
-            <div className='mt-[2rem]'>
-              <h2 className='text-5xl font-normal '>{heading2}</h2>
-              <p className='py-5 text-xl '>{message2}</p>
             </div>
  
         </div>
