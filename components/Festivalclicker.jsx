57dc86afc7dda4e5a18fba273af5ba961d5b6b00
Wil Hesk <wilhesk@gmail.com>
2024-04-02 09:13:46 +0100

    clicker styling

--- components/Festivalclicker.jsx
+++ components/Festivalclicker.jsx
@@ -5,13 +5,13 @@ import Link from 'next/link';
 const Festivalclicker = ({ festivaltitle, festivalsubtitle, festivalp1, festivalclick }) => {
   return (
     <section id='festival' className='flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img'>
-      <div  className='p-5 h-5/6 rounded-3xl flex items-center justify-center w-screen max-w-screen-lg bg-dkgrey bg-opacity-80 backdrop-blur-md flex-col md:flex-row'>
+      <div  className='h-5/6 rounded-3xl flex items-center justify-center w-screen max-w-screen-lg bg-dkgrey bg-opacity-80 backdrop-blur-md flex-col md:flex-row'>
         
-        <div className='p-5 min-w-1/2 max-w-1/2'>
-          <div className='p-5 rounded-xl flex justify-center flex-col bg-white border-4 hover:border-pink border-dkpink backdrop-blur-md'>
+        <div className='py-4'>
+          <div className='rounded-xl flex justify-center flex-col bg-white border-4 hover:border-pink border-dkpink backdrop-blur-md sm:p-10'>
             <Link href='/festival'>
               <a className='px-4'>
-              <div className='pt-4 pb-6'>
+              <div className='pt-10 pb-6'>
                 <Image
                   src='/Coping-festival-logo-grey-2024.svg'
                   alt='coping festival skateboarder kickflipping stairset at graystone action sports' 
@@ -22,7 +22,7 @@ const Festivalclicker = ({ festivaltitle, festivalsubtitle, festivalp1, festival
               </div> 
                 <h1 className='py-1 font-bold text-xl uppercase leading-6'>{festivaltitle}</h1> 
                 <h1 className='py-1 font-medium text-l text-dkpink leading-5'>{festivalsubtitle}</h1> 
-                <p className='py-1 font-light text-sm leading-6'>{festivalp1}</p>
+                <p className='py-1 font-light text-md leading-6'>{festivalp1}</p>
                 <p className='pt-1 pb-4 font-bold text-dkpink'>{festivalclick}</p> 
               </a>
             </Link>            
--- components/Strategyclicker.jsx
+++ components/Strategyclicker.jsx
@@ -5,13 +5,13 @@ import Link from 'next/link';
 const Strategyclicker = ({ strategytitle, strategysubtitle, strategyp1, strategyclick }) => {
   return (
     <section id='festival' className='flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img'>
-      <div  className='p-5 h-5/6 rounded-3xl flex items-center justify-center w-screen max-w-screen-lg bg-dkgrey bg-opacity-80 backdrop-blur-md flex-col md:flex-row'>
+      <div  className='h-5/6 rounded-3xl flex items-center justify-center w-screen max-w-screen-lg bg-dkgrey bg-opacity-80 backdrop-blur-md flex-col md:flex-row'>
         
-        <div className='p-5 min-w-1/2 max-w-1/2'>
-          <div className='p-5 rounded-xl flex justify-center flex-col bg-white hover:bg-pink hover:text-dkgrey border-2 border-pink backdrop-blur-md'>
-            <Link href='/strategy'>
+        <div className='py-4'>
+          <div className='rounded-xl flex justify-center flex-col bg-white border-4 hover:border-pink border-dkpink backdrop-blur-md sm:p-10'>
+            <Link href='/festival'>
               <a className='px-4'>
-                <div className='pt-4 pb-6'>
+              <div className='pt-10 pb-6'>
                   <Image
                     src='/Coping-strategy-logo-grey-2024.svg'
                     alt='coping festival skateboarder kickflipping stairset at graystone action sports' 
@@ -22,7 +22,7 @@ const Strategyclicker = ({ strategytitle, strategysubtitle, strategyp1, strategy
                 </div> 
                 <h1 className='py-1 font-bold text-xl uppercase leading-6'>{strategytitle}</h1> 
                 <h1 className='py-1 font-medium text-l text-dkpink leading-5'>{strategysubtitle}</h1> 
-                <p className='py-1 font-light text-sm leading-6'>{strategyp1}</p>
+                <p className='py-1 font-light text-md leading-6'>{strategyp1}</p>
                 <p className='pt-1 pb-4 font-bold text-dkpink'>{strategyclick}</p> 
               </a>
             </Link>            
